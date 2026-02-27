58b62db8fab9e92d47c1dc5817ce35d5cb1df6d1
Ryan Martinez <ryanmartinez2021@gmail.com>
2026-02-27 00:50:46 -0800

    copy from me

--- a2/Code.java
+++ a2/Code.java
@@ -18,120 +18,110 @@ import com.jogamp.common.nio.Buffers;
 import org.joml.Matrix4f;
 
 public class Code extends JFrame implements  GLEventListener {
-  private GLCanvas canvas;
+  private GLCanvas myCanvas;
   private int renderingProgram;
-  private int[] vao = new int[1];
+  private int vao[] = new int[1];
   private int vbo[] = new int[2];
-  private float cameraX, cameraY, cameraZ,
-  cubeLocX, cubeLocY, cubeLocZ;
-  private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);
-  private Matrix4f pMat= new Matrix4f();
-  private Matrix4f vMat= new Matrix4f();
-  private Matrix4f mMat= new Matrix4f();
-  private Matrix4f mvMat = new Matrix4f();
+  private float cameraX, cameraY, cameraZ;
+  private float cubeLocX, cubeLocY, cubeLocZ;
+
+  // allocate variables for display() function
+  private FloatBuffer vals = Buffers.newDirectFloatBuffer(16);  // buffer for transfering matrix to uniform
+  private Matrix4f pMat = new Matrix4f();  // perspective matrix
+  private Matrix4f vMat = new Matrix4f();  // view matrix
+  private Matrix4f mMat = new Matrix4f();  // model matrix
+  private Matrix4f mvMat = new Matrix4f(); // model-view matrix
   private int mvLoc, pLoc;
   private float aspect;
-  double elapsedTime, startTime, tf;
-  public static void main(String[] args) {
-    new Code();
+  private double tf;
+  private double startTime;
+  private double elapsedTime;
 
-  }
-
-  public Code() {
-    setTitle("155 Assignment 2");
-    setSize(800, 600);
-    canvas = new GLCanvas();
-    canvas.addGLEventListener(this);
-    this.add(canvas);
-    setVisible(true);
-    Animator animator = new Animator(canvas);
+  public Code()
+  {	setTitle("Chapter 4 - program 1c");
+    setSize(600, 600);
+    myCanvas = new GLCanvas();
+    myCanvas.addGLEventListener(this);
+    this.add(myCanvas);
+    this.setVisible(true);
+    Animator animator = new Animator(myCanvas);
     animator.start();
   }
-  @Override
-  public void init(GLAutoDrawable glAutoDrawable) {
-    GL4 gl = (GL4) GLContext.getCurrentGL();
-    renderingProgram = a2.Utils.createShaderProgram("shaders\\vertex.glsl", "shaders\\fragment.glsl");
-    setupVertices();
-    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
-    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
-    startTime = System.currentTimeMillis();
 
-  }
-
-  private void setupVertices() {
-    GL4 gl = (GL4) GLContext.getCurrentGL();
-// 36 vertices of the 12 triangles making up a 2 x 2 x 2 cube centered at the origin
-    float[ ] vertexPositions =
-            { -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
-                    1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
-                    -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
-                    -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
-                    1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
-            };
-    gl.glGenVertexArrays(vao.length, vao, 0);
-    gl.glBindVertexArray(vao[0]);
-    gl.glGenBuffers(vbo.length, vbo, 0);
-    gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertexPositions);
-    gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
-
-  }
-
-  @Override
-  public void dispose(GLAutoDrawable glAutoDrawable) {
-
-  }
-
-  @Override
-  public void display(GLAutoDrawable glAutoDrawable) {
-    GL4 gl = (GL4) GLContext.getCurrentGL();
+  public void display(GLAutoDrawable drawable)
+  {	GL4 gl = (GL4) GLContext.getCurrentGL();
     gl.glClear(GL_DEPTH_BUFFER_BIT);
     gl.glClear(GL_COLOR_BUFFER_BIT);
 
-
     gl.glUseProgram(renderingProgram);
-// get references to the uniform variables for the MV and projection matrices
+
     mvLoc = gl.glGetUniformLocation(renderingProgram, "mv_matrix");
     pLoc = gl.glGetUniformLocation(renderingProgram, "p_matrix");
-// build perspective matrix. This one has fovy=60, aspect ratio matches the screen window.
-// Values for near and far clipping planes can vary as discussed in Section 4.9
-    aspect = (float) canvas.getWidth() / (float) canvas.getHeight();
+
+    aspect = (float) myCanvas.getWidth() / (float) myCanvas.getHeight();
     pMat.setPerspective((float) Math.toRadians(60.0f), aspect, 0.1f, 1000.0f);
-// build view matrix, model matrix, and model-view matrix
+
     vMat.translation(-cameraX, -cameraY, -cameraZ);
 
     elapsedTime = System.currentTimeMillis() - startTime;
-    tf = elapsedTime / 1000.0;
+    tf = elapsedTime/1000.0;  // time factor
     mMat.identity();
-    mMat.translate((float)Math.sin(.35f*tf)*2.0f, (float)Math.sin(.52f*tf)*2.0f, (float)Math.sin(.7f*tf)*2.0f);
+    mMat.translate((float)Math.sin(.35f*tf)*2.0f, (float)Math.sin(.52f*tf)*2.0f, (float)Math.sin(.70f*tf)*2.0f);
     mMat.rotateXYZ(1.75f*(float)tf, 1.75f*(float)tf, 1.75f*(float)tf);
-
 
     mvMat.identity();
     mvMat.mul(vMat);
     mvMat.mul(mMat);
-// copy perspective and MV matrices to corresponding uniform variables
+
     gl.glUniformMatrix4fv(mvLoc, 1, false, mvMat.get(vals));
     gl.glUniformMatrix4fv(pLoc, 1, false, pMat.get(vals));
-// associate VBO with the corresponding vertex attribute in the vertex shader
+
     gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     gl.glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
     gl.glEnableVertexAttribArray(0);
-// adjust OpenGL settings and draw model
+
     gl.glEnable(GL_DEPTH_TEST);
     gl.glDepthFunc(GL_LEQUAL);
+
     gl.glDrawArrays(GL_TRIANGLES, 0, 36);
   }
 
-  @Override
-  public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
-
+  public void init(GLAutoDrawable drawable)
+  {	GL4 gl = (GL4) drawable.getGL();
+    startTime = System.currentTimeMillis();
+    renderingProgram = Utils.createShaderProgram("code/vertShader.glsl", "code/fragShader.glsl");
+    setupVertices();
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
   }
+
+  private void setupVertices()
+  {	GL4 gl = (GL4) GLContext.getCurrentGL();
+    float[] vertexPositions =
+            {	-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+                    1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+                    1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+                    1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+                    1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+                    -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+                    -1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+                    -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+                    -1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+                    1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+                    -1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+                    1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+            };
+
+    gl.glGenVertexArrays(vao.length, vao, 0);
+    gl.glBindVertexArray(vao[0]);
+    gl.glGenBuffers(vbo.length, vbo, 0);
+
+    gl.glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    FloatBuffer vertBuf = Buffers.newDirectFloatBuffer(vertexPositions);
+    gl.glBufferData(GL_ARRAY_BUFFER, vertBuf.limit()*4, vertBuf, GL_STATIC_DRAW);
+  }
+
+  public static void main(String[] args) { new Code(); }
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
+  public void dispose(GLAutoDrawable drawable) {}
 }
